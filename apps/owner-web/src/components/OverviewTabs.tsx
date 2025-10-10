4c931e78b8fc7cf4a29fa6963cc2e94a34604b1c
Nick Schneble <nschneble@users.noreply.github.com>
2025-10-10 16:30:01 -0400

    add type to import

--- apps/owner-web/src/components/OverviewTabs.tsx
+++ apps/owner-web/src/components/OverviewTabs.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./OverviewTabs.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
 export type TabKey =
