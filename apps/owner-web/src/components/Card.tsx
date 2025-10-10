bf936343ac3d2153063d09be65ace689b6125dd0
Nick Schneble <nschneble@users.noreply.github.com>
2025-10-10 16:29:16 -0400

    make more card colors configurable

--- apps/owner-web/src/components/Card.tsx
+++ apps/owner-web/src/components/Card.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Card.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 export type CardSize = "025x" | "05x" | "1x" | "2x" | "3x" | "4x";
 export type TitleAlignment = "left" | "center";
 
 type Props = {
+  backgroundColor?: string;
   title: string;
+  titleColor?: string;
   icon?: IconProp;
   iconColor?: string;
   header?: React.ReactNode;
@@ -18,9 +20,11 @@ type Props = {
 };
 
 export default function Card({
+  backgroundColor = "var(--light-color)",
   title,
+  titleColor = "var(--dark-color)",
   icon,
-  iconColor = "#9dda8f",
+  iconColor = "var(--alt-theme-color)",
   header,
   align = "left",
   width = "1x",
@@ -28,15 +32,23 @@ export default function Card({
   children,
 }: Props) {
   return (
-    <section className={`overview-card card-w-${width} card-h-${height}`}>
+    <section
+      className={`overview-card card-w-${width} card-h-${height}`}
+      style={{ backgroundColor: backgroundColor }}
+    >
       <header className="card-header">
         <div className={`card-title card-align-${align}`}>
           {icon && (
-            <span className="card-icon" style={{ backgroundColor: iconColor }}>
+            <span
+              className="card-icon"
+              style={{ backgroundColor: iconColor, color: backgroundColor }}
+            >
               <FontAwesomeIcon icon={icon} size="1x" />
             </span>
           )}
-          <h3 className={`card-align-${align}`}>{title}</h3>
+          <h3 className={`card-align-${align}`} style={{ color: titleColor }}>
+            {title}
+          </h3>
         </div>
         {header && <div className="card-header-text">{header}</div>}
       </header>
